eb7db4b1e4d6e011725be47d8c5bb3848c2f7b9e
rolite-root <sushantkumaryadav912@gmail.com>
2025-03-25 11:32:13 +0530

    first commit

---
+++ src/main/java/com/example/SmartHealthChain/SmartHealthChainApplication.java
@@ -0,0 +1,13 @@
+package com.example.SmartHealthChain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHealthChainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartHealthChainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SmartHealthChain/SmartHealthChainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SmartHealthChain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHealthChainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
