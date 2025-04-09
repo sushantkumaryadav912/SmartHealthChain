baa1a599880a1333c2762891bd860fb951e00712
rolite-root <sushantkumaryadav912@gmail.com>
2025-04-09 10:40:28 +0530

    Ui file setup

---
+++ src/main/java/com/smarthealthchain/MainFXApp.java
@@ -0,0 +1,11 @@
+package com.smarthealthchain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainFXApp {
+    public static void main(String[] args) {
+        SpringApplication.run(MainFXApp.class, args);
+    }
+}
---
+++ src/main/java/com/smarthealthchain/ui/controller/DashboardController.java
@@ -0,0 +1,4 @@
+package com.smarthealthchain.ui.controller;
+
+public class DashboardController {
+}
